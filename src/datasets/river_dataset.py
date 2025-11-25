8fa4cf7629f636768babc1eda73099021dd49aef
KubaCzech <kuba.aleksander.czech@gmail.com>
2025-11-26 00:49:57 +0100

    adding river datasets: electricity, airlines and forest cover

---
+++ src/datasets/river_dataset.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+from enum import Enum
+from river import datasets
+from .base import BaseDataset
+from .utils import generate_data_from_dataset_stream
+
+
+class RiverDatasetType(Enum):
+    ELECTRICITY = "Electricity"
+    AIRLIENES = "Airlines"
+    FOREST_COVER_TYPE = "Forest"
+
+
+class RiverDataset(BaseDataset):
+    def __init__(self, name: str = RiverDatasetType.ELECTRICITY.value):
+        self.dataset_name = name
+        if self.dataset_name == RiverDatasetType.ELECTRICITY.value:
+            self.dataset = datasets.Elec2()
+        elif self.dataset_name == RiverDatasetType.AIRLIENES.value:
+            self.dataset = datasets.Airlines()
+        elif self.dataset_name == RiverDatasetType.FOREST_COVER_TYPE.value:
+            self.dataset = datasets.ForestCoverType()
+        else:
+            raise ValueError(f"Unsupported dataset name: {self.dataset_name}")
+
+    @property
+    def name(self) -> str:
+        return f"{self.dataset_name.lower}_drift"
+
+    @property
+    def display_name(self) -> str:
+        return f"{self.dataset_name} Drift"
+
+    def generate(
+        self,
+        size_of_block=2000,
+        starting_point=None,
+        random_seed=42,
+    ):
+        # TODO: number of features
+        size_of_dataset = self.dataset.n_samples
+        np.random.seed(random_seed)
+
+        if starting_point is None:
+            starting_point = np.random.randint(0, size_of_dataset - size_of_block)
+        else:
+            starting_point = max(0, min(starting_point, size_of_dataset - size_of_block))
+
+        stream = self.dataset.skip(starting_point)
+
+        return generate_data_from_dataset_stream(stream, size_of_block, n_features=len(self.dataset.features))
--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 
 def generate_river_data(river_stream, n_samples_before, n_samples_after, n_features=2):
+    # TODO: return feature names
     """
     Helper function to generate data from a river stream.
 
@@ -42,4 +43,43 @@ def generate_river_data(river_stream, n_samples_before, n_samples_after, n_featu
     y = pd.Series(y_all, name='Y')
     drift_point = n_samples_before
 
+    # return X, y, drift_point, X.columns.tolist()
     return X, y, drift_point
+
+
+def generate_data_from_dataset_stream(stream, size_of_block):
+    """
+    Helper function to generate data from ready river dataset.
+
+    Parameters
+    ----------
+    stream : river stream object
+        The dataset in form of river stream
+    size_of_block : int
+        Number of samples to extract
+
+    Returns
+    -------
+    tuple
+        (X, y, drift_point, feature_names)
+    """
+    # TODO add number of features as parameter ???
+    # TODO will this be compatible with generate_river_data ??
+    X_all = []
+    y_all = []
+
+    try:
+        for x, y_val in itertools.islice(stream, size_of_block):
+            features = [i for i in x]
+            X_all.append(features)
+            y_all.append(y_val)
+
+    except (StopIteration, KeyError) as e:
+        print(f"Warning: Stream generation failed: {e}")
+        return np.array([]), np.array([])
+
+    n_features = len(features)
+    X = pd.DataFrame(X_all, columns=[f'X{i+1}' for i in range(n_features)])
+    y = pd.Series(y_all, name='Y')
+
+    return X, y
