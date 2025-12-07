2142cbbbd6b015108e3df12bb92683e81e8c8796
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2025-12-07 19:20:12 +0100

    feat: Remove unused datasets from the list

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -2,8 +2,6 @@ from .base import BaseDataset
 from .sea_drift import SeaDriftDataset
 from .hyperplane_drift import HyperplaneDriftDataset
 from .controlled_concept_drift import ControlledConceptDriftDataset
-from .csv_dataset import CSVDataset
-from .random_rbf_drift import RandomRBFDriftDataset
 from .sdbm_rbf_drift import SDBMRBFDriftDataset
 
 from .river_dataset import RiverDataset, RiverDatasetType
@@ -15,9 +13,9 @@ def load_datasets():
         SeaDriftDataset(),
         HyperplaneDriftDataset(),
         ControlledConceptDriftDataset(),
-        RandomRBFDriftDataset(),
+        # RandomRBFDriftDataset(),
         SDBMRBFDriftDataset(),
-        CSVDataset(),
+        # CSVDataset(),
         RiverDataset(RiverDatasetType.ELECTRICITY.value),
         # RiverDataset(RiverDatasetType.AIRLIENES.value),
         # RiverDataset(RiverDatasetType.FOREST_COVER_TYPE.value)
