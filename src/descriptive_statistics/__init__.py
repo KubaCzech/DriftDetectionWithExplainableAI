39ce73e803aebef6a0dd1559eb7d6b1f8cec808c
KubaCzech <kuba.aleksander.czech@gmail.com>
2025-12-27 01:43:26 +0100

    __init__ fix

--- src/descriptive_statistics/__init__.py
+++ src/descriptive_statistics/__init__.py
@@ -1,2 +1,2 @@
-from descriptive_statistics import DescriptiveStatisticsDriftDetector, StatisticsType
-from visualization import PlotOptions, plot_boxplot, plot_histogram, plot_kde, plot_ecdf, plot_violin, plot_qq
+from .descriptive_statistics import DescriptiveStatisticsDriftDetector, StatisticsType
+from .visualization import PlotOptions, plot_boxplot, plot_histogram, plot_kde, plot_ecdf, plot_violin, plot_qq
