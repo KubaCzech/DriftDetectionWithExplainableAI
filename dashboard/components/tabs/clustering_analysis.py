eb7e31ff02e56baf35bfe4292ecb39fff17cd83c
KubaCzech <kuba.aleksander.czech@gmail.com>
2025-12-31 01:19:55 +0100

    adding new clustering visualizations to dashboard

--- dashboard/components/tabs/clustering_analysis.py
+++ dashboard/components/tabs/clustering_analysis.py
@@ -1,10 +1,11 @@
 import traceback
+import numpy as np
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
 from src.clustering.clustering import ClusterBasedDriftDetector
-from src.clustering.visualization import plot_clusters_by_class
+from src.clustering.visualization import plot_clusters_by_class, plot_drift_clustered, plot_centers_shift
 
 
 def _display_intro():
@@ -56,17 +57,129 @@ def _display_drift_details(details):
     )
 
 
-def _display_cluster_plot(X_before, X_after, y_before, y_after, detector):
-    st.subheader("Cluster Visualization")
+def _display_visualization(X_before, X_after, y_before, y_after, labels_old, labels_new, viz_type='cluster by class'):
+    plot_func = CLUSTER_PLOT_FUNCTIONS.get(viz_type, plot_clusters_by_class)
+    plot_func(X_before, X_after, y_before, y_after, labels_old, labels_new)
+
+
+def _display_centroid_shift_table(detector):
+    # TODO: add docstring
+    centroid_shifts = (
+        pd.DataFrame.from_dict(detector.cluster_shifts, orient="index", columns=["Shift"])
+        .reset_index()
+        .rename(columns={"index": "Cluster ID"})
+    )
+    centroid_shifts["Drifted"] = centroid_shifts["Shift"].apply(
+        lambda x: isinstance(x, float) and x > detector.thr_centroid_shift or x == 'appeared' or x == 'disappeared'
+    )
+
+    st.dataframe(
+        centroid_shifts,
+        column_config={
+            "Cluster ID": st.column_config.TextColumn("Cluster ID"),
+            "Shift": st.column_config.TextColumn("Cluster Shift", help="How much did the cluster shift"),
+            "Drifted": st.column_config.CheckboxColumn(
+                "Drifted",
+                help=f"True if the cluster centroid shifted above the threshold ({detector.thr_centroid_shift})",
+                disabled=True,
+            ),
+        },
+        width="stretch",
+        hide_index=True,
+    )
+
+
+def _display_stats_shift_table(detector):
+    # TODO: add docstring
+    # TODO: add better formatting
+    def stats_dict_to_df(stats_dict: dict) -> pd.DataFrame:
+        """
+        Converts a nested dict {cluster -> feature -> stat -> value}
+        into a flat pandas DataFrame.
+        """
+        records = []
+        for cluster_id, features in stats_dict.items():
+            row = {"Cluster": cluster_id}
+            for feature_name, stats in features.items():
+                for stat_name, value in stats.items():
+                    row[f"{feature_name} | {stat_name}"] = value
+            records.append(row)
+
+        df = pd.DataFrame(records)
+        return df
+
+    stats_shifts = stats_dict_to_df(detector.stats_shifts)
+    st.dataframe(
+        stats_shifts,
+        use_container_width=True,
+    )
+
+
+def _display_stats_table(detector):
+    # TODO: add docstring
+    # TODO: add better formatting
+    def flatten_multiindex_columns(df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy()
+        df.columns = [f"{time} | {feature} | {stat}" for time, feature, stat in df.columns]
+        return df
+
+    stats = flatten_multiindex_columns(detector.stats_combined)
+    st.subheader("Cluster Descriptive Statistics")
+    st.dataframe(
+        stats,
+        use_container_width=True,
+    )
+
+
+def _display_table(detector, table_type):
+    table_func = CLUSTER_TABLE_FUNCTIONS.get(table_type, _display_centroid_shift_table)
+    table_func(detector)
+
+
+CLUSTER_PLOT_FUNCTIONS = {
+    'clusters by class': plot_clusters_by_class,
+    'clusters overall': plot_drift_clustered,
+    'centroid shifts (plot)': plot_centers_shift,
+}
+
+CLUSTER_TABLE_FUNCTIONS = {
+    'centroid shifts (table)': _display_centroid_shift_table,
+    'stats shifts': _display_stats_shift_table,
+    'stats table': _display_stats_table,
+}
+
+
+def _display(X_before, X_after, y_before, y_after, detector):
+    st.subheader("Clustering Visualization")
 
     labels_old = detector.cluster_labels_old
     labels_new = detector.cluster_labels_new
 
     if labels_old is not None and labels_new is not None:
-        plot_clusters_by_class(X_before, X_after, y_before, y_after, labels_old, labels_new)
-        fig = plt.gcf()
-        st.pyplot(fig)
-        plt.close(fig)
+        col_viz_options, _ = st.columns([1, 2])
+        with col_viz_options:
+            # TODO: wymyslic opisy
+            _type = st.selectbox(
+                "TODO",
+                options=[
+                    'clusters by class',
+                    'clusters overall',
+                    'centroid shifts (plot)',
+                    'centroid shifts (table)',
+                    'stats table',
+                    'stats shifts',
+                ],
+                index=0,  # Default to 'cluster by class'
+                key='viz_type_selector',
+                help="TODO",
+            )
+        if _type in CLUSTER_PLOT_FUNCTIONS:
+            _display_visualization(X_before, X_after, y_before, y_after, labels_old, labels_new, viz_type=_type)
+            fig = plt.gcf()
+            st.pyplot(fig)
+            plt.close(fig)
+        elif _type in CLUSTER_TABLE_FUNCTIONS:
+            _display_table(detector, table_type=_type)
     else:
         st.warning("Cluster labels not available for plotting.")
 
@@ -97,7 +210,7 @@ def render_clustering_analysis_tab(X_before, y_before, X_after, y_after):
 
             _display_drift_status(drift_flag)
             _display_drift_details(details)
-            _display_cluster_plot(X_before, X_after, y_before, y_after, detector)
+            _display(X_before, X_after, y_before, y_after, detector)
 
         except Exception as e:
             st.error(f"An error occurred during drift detection: {e}")
